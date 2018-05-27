b437c9b9dcde694e1b49c23c8aaf55634d707813
gkandale <gkandale@cisco.com>
2018-05-26 23:20:21 -0400

    adding gooogle keys to prod

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,15 @@
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+    googleClientID:
+        '252209568841-sj0cbk5famql7um8jq51sik4j1bk4md1.apps.googleusercontent.com',
+    googleClientSecret: 'bH-Eg4___yVncdT6u2bk-LWY',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    jestTimeout: 30000,
+    redisUrl: 'redis://127.0.0.1:6379'
 };
+// module.exports = {
+//   googleClientID: process.env.GOOGLE_CLIENT_ID,
+//   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//   mongoURI: process.env.MONGO_URI,
+//   cookieKey: process.env.COOKIE_KEY
+// };
